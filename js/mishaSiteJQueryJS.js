f9da38c5649e07ebebfbe1e7f383a7d9d9469f32
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 13:48:24 -0400

    Added skeleton for hover sections, fixed bug with jumbotron fadein

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  $(.jumbotron).fadeIn("slow");
+  $(".jumbotron").fadeIn("slow");
+  $("#about").hover(function(){
+
+  });
+  $("#projects").hover(function(){
+
+  });
+  $("#mapping").hover(function(){
+
+  });
+  $("#contact").hover(function(){
+
+  });
 });
