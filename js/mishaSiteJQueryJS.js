989893ab59d24c6c2d9495f7dacf478009523627
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 19:13:01 -0400

    Trying opacity animation with jquery

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
   $("#about").hover(function() {
-      $(this).animate(function() {
-          background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')";
-          //alert("Hi");
-        }), // remove comma
-        //,
-        //"slow"
-      //)
-    //  ;
-
-    //},
-    function() {
-      alert("Bye");
-    }
-  );
+      $(this).animate({ opacity: 1 }, { duration: 3000 });
+          //background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+    },
+      $(this).animate({ opacity: 0 }, { duration: 3000 });
+    );
 
 //  $("#projects").hover(function() {
 
