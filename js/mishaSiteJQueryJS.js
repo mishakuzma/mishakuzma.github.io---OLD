4e0f31f446e3af55658848f94367728faca81087
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 18:57:08 -0400

    Isolate problem by commenting out other stuff

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -4,23 +4,25 @@ $(document).ready(function() {
       //$(this).animate(function() {
           //background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
           alert("Hi");
-        }
-        ,
-        "slow");
+        }, // remove comma
+        //,
+        //"slow"
+      //)
+    //  ;
 
-    },
+    //},
     function() {
       alert("Bye");
   //  }
   );
 
-  $("#projects").hover(function() {
+//  $("#projects").hover(function() {
 
-  });
-  $("#mapping").hover(function() {
+//  });
+//  $("#mapping").hover(function() {
 
-  });
-  $("#contact").hover(function() {
+//  });
+//  $("#contact").hover(function() {
 
-  });
+//  });
 });
