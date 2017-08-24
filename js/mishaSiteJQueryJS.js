8c0ba3c0e947d4c80bcca25883ab9d815333e8ea
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 19:26:03 -0400

    Solution test with bg as seperate div

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
-  $("#about").hover(function() {
-      $(this).animate({ opacity: 1 }, { duration: 3000 });
+  $("#bgAbout").hover(function() {
+      $(this).animate({ opacity: 1 }, { duration: 1000 });
           //background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
     },
       function() {
-        $(this).animate({ opacity: 0 }, { duration: 3000 });
+        $(this).animate({ opacity: 0 }, { duration: 1000 });
       }
     );
 
