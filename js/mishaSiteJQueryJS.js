dae65a869e3973523ee244719a093831a8316c40
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 19:28:22 -0400

    Bug test fix

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
-  $("#bgAbout").hover(function() {
-      $(this).animate({ opacity: 1 }, { duration: 1000 });
+  $("#About").hover(function() {
+      $("#bgAbout").animate({ opacity: 1 }, { duration: 1000 });
           //background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
     },
       function() {
-        $(this).animate({ opacity: 0 }, { duration: 1000 });
-      }
-    );
+        $("#bgAbout").animate({ opacity: 0 }, { duration: 1000 });
+      });
 
 //  $("#projects").hover(function() {
 
