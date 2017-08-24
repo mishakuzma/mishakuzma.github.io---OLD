319fdc50bd2a42b9d968951bfb9cb556b44c15b7
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 17:10:49 -0400

    JS changes -Added another function to about animate - Beautified

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
   $("#about").hover(function() {
-    $(this).animate(function() {
-      background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
-    },
-    "slow");
+      $(this).animate(function() {
+          background - image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+        },
+        "slow");
 
-  },
-  alert("Bye");
-);
+    },
+    function() {
+      alert("Bye");
+    });
 
   $("#projects").hover(function() {
 
@@ -16,7 +17,7 @@ $(document).ready(function() {
   $("#mapping").hover(function() {
 
   });
-    $("#contact").hover(function() {
+  $("#contact").hover(function() {
 
   });
 });
