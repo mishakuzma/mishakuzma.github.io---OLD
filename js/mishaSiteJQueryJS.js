039f7ee62c4e6e6e411bd30fc9b29f8fa6caf485
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 19:04:53 -0400

    Put semicolon at end of background image

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
   $("#about").hover(function() {
       $(this).animate(function() {
-          background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+          background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')";
           //alert("Hi");
         }), // remove comma
         //,
