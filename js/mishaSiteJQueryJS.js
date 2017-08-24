e0e86f883d7fd6907cfaa2707350ec271281b9a1
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 18:54:42 -0400

    Test problem with hover.

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
   $("#about").hover(function() {
-      $(this).animate(function() {
-          background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
-        },
+      //$(this).animate(function() {
+          //background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+          alert("Hi");
+        }
+        ,
         "slow");
 
     },
     function() {
       alert("Bye");
-    });
+  //  }
+  );
 
   $("#projects").hover(function() {
 
