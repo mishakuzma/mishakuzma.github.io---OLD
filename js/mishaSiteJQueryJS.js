b6a85fd3f299512f77a744122cf68fb902e685aa
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 16:31:24 -0400

    Fixed brackets on jquery

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
-  $("#about").hover(function(){
-    $(this).animate(function(){
-      background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+  $("#about").hover(function() {
+    $(this).animate(function() {
+      background - image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
     },
-      "slow");
+    "slow");
 
-    });
-  });
-  $("#projects").hover(function(){
+});
 
-  });
-  $("#mapping").hover(function(){
+$("#projects").hover(function() {
 
-  });
-  $("#contact").hover(function(){
+});
+$("#mapping").hover(function() {
 
-  });
+});
+$("#contact").hover(function() {
+
+});
 });
