b42f5fcc00c821246f6da99916edeb57bd9f5275
Misha Kuzma <mishakuzma@gmail.com>
2017-08-24 16:59:40 -0400

    Html and Js changes - put navbar text in center - changed spacing for js

--- js/mishaSiteJQueryJS.js
+++ js/mishaSiteJQueryJS.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   $(".jumbotron").fadeIn("slow");
   $("#about").hover(function() {
     $(this).animate(function() {
-      background - image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
+      background-image: "url('http://mishakuzma.weebly.com/uploads/6/6/2/4/66248371/background-images/1834286649.jpg')"
     },
     "slow");
 
-});
+  });
 
-$("#projects").hover(function() {
+  $("#projects").hover(function() {
 
-});
-$("#mapping").hover(function() {
+  });
+  $("#mapping").hover(function() {
 
-});
-$("#contact").hover(function() {
+  });
+    $("#contact").hover(function() {
 
-});
+  });
 });
